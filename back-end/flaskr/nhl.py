b2379eaa9aa01c647e2b1c6f88ab508a936f3f83
jaideep26 <jaideep.sidhu97@gmail.com>
2025-01-26 15:12:09 -0500

    finish /nhl/schedule endpoint

--- back-end/flaskr/nhl.py
+++ back-end/flaskr/nhl.py
@@ -13,14 +13,56 @@ def get_schedule():
 
         if fetched_schedule_response.status_code == 200:
             response = fetched_schedule_response.json()
-
+            game_week = get_game_week(response)
             #TODO - create a custom response
             data = {
-                "gameWeek": response.get("gameWeek")
+                "gameWeek": game_week
             }
             return jsonify({"success": True, "data": data})
         else:
             return jsonify({"success": False, "error": f"Request failed with status code {response.status_code}"})
     except requests.exceptions.RequestException as e:
-        # Handle request exceptions (e.g., network errors)
         return jsonify({"success": False, "error": str(e)})
+
+def get_game_week(response):
+    fetched_game_week = response.get("gameWeek")
+    game_days = []
+
+    for fetched_game_day in fetched_game_week:
+        game_days.append(get_game_day(fetched_game_day))
+
+    return game_days
+
+def get_game_day(game_day):
+    games = []
+    for game in game_day.get("games"):
+        games.append(get_game_details(game))
+
+    return {
+        "date": game_day.get("date"),
+        "games": games,
+        "numberOfGames": game_day.get("numberOfGames")
+    }
+
+def get_game_details(game):
+    return {
+        "awayTeam": get_team_details(game.get("awayTeam")),
+        "gameScheduleState": game.get("gameScheduleState"),
+        "gameState": game.get("gameState"),
+        "homeTeam": get_team_details(game.get("homeTeam")),
+        "id": game.get("id"),
+        "periodDescriptor": game.get("periodDescriptor"),
+        "startTimeUTC": game.get("startTimeUTC"),
+        "venue": game.get("venue").get("default")
+    }
+
+def get_team_details(team):
+    return {
+        "abbreviation": team.get("abbrev"),
+        "commonName": team.get("commonName").get("default"),
+        "darkLogo": team.get("darkLogo"),
+        "id": team.get("id"),
+        "logo": team.get("logo"),
+        "placeName": team.get("placeName").get("default"),
+        "score": team.get("score"),
+    }
