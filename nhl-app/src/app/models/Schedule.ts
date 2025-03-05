46bbf1428e773c51c459e02b06784effaea5c928
Jaideep <jaideep.sidhu@levio.ca>
2025-03-05 10:02:07 -0500

    Refactor Schedule model to simplify structure and remove unused properties

--- nhl-app/src/app/models/Schedule.ts
+++ nhl-app/src/app/models/Schedule.ts
@@ -1,18 +1,10 @@
 //TODO - design model for schedule based on API response
 
 export interface Schedule {
-  nextStartDate: string;
-  previousStartDate: string;
-  gameWeek: GameWeek[];
-  oddsPartners: OddsPartner[];
-  preSeasonStartDate: string;
-  regularSeasonStartDate: string;
-  regularSeasonEndDate: string;
-  playoffEndDate: string;
-  numberOfGames: number;
+  gameWeek: GameDay[];
 }
 
-export interface GameWeek {
+export interface GameDay {
   date: string;
   dayAbbrev: string;
   numberOfGames: number;
@@ -21,37 +13,12 @@ export interface GameWeek {
 
 export interface Game {
   id: number;
-  season: number;
   gameType: number;
-  venue: Venue;
-  neutralSite: boolean;
   startTimeUTC: string;
-  easternUTCOffset: string;
-  venueUTCOffset: string;
-  venueTimezone: string;
   gameState: string;
   gameScheduleState: string;
-  tvBroadcasts: TvBroadcast[];
   awayTeam: Team;
   homeTeam: Team;
-  gameOutcome: GameOutcome;
-  winningGoalie?: Player;
-  winningGoalScorer?: Player;
-  specialEvent?: string;
-  gameCenterLink: string;
-}
-
-export interface Venue {
-  default: string;
-  es?: string;
-  fr?: string;
-}
-
-export interface TvBroadcast {
-  id: number;
-  market: string;
-  countryCode: string;
-  network: string;
 }
 
 export interface Team {
@@ -69,28 +36,10 @@ export interface Team {
   abbrev: string;
   logo: string;
   darkLogo: string;
-  radioLink: string;
-  awaySplitSquad?: boolean;
-  homeSplitSquad?: boolean;
-}
-
-export interface GameOutcome {
-  lastPeriodType: string;
 }
 
 export interface Player {
   playerId: number;
   firstInitial: string;
   lastName: string;
-}
-
-export interface OddsPartner {
-  partnerId: number;
-  country: string;
-  name: string;
-  imageUrl: string;
-  siteUrl?: string;
-  bgColor: string;
-  textColor: string;
-  accentColor: string;
 }
