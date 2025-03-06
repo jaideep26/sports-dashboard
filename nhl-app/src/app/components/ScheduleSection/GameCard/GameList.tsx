af97221975763e3117fd9716bfbd7dc79d157700
Jaideep <jaideep.sidhu@levio.ca>
2025-03-06 09:09:09 -0500

    Enhance GameCard component layout with team logos

--- nhl-app/src/app/components/ScheduleSection/GameCard/GameList.tsx
+++ nhl-app/src/app/components/ScheduleSection/GameCard/GameList.tsx
@@ -1,5 +1,11 @@
 import { Game } from "@/app/models/Schedule";
-import { Card, CardActionArea, CardContent, Typography } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  Typography,
+} from "@mui/material";
 import dayjs from "dayjs";
 
 export type GameCardProps = {
@@ -10,10 +16,36 @@ export const GameCard = ({ game }: GameCardProps) => {
   return (
     <Card key={game.id} sx={{ minWidth: "15%" }}>
       <CardActionArea>
-        <CardContent sx={{ display: "flex", justifyContent: "space-between" }}>
-          <Typography>
-            {game.awayTeam.abbrev} @ {game.homeTeam.abbrev}
-          </Typography>
+        <CardContent
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              textAlign: "center",
+              gap: 1,
+            }}
+          >
+            <Box component="img" src={game.awayTeam.logo} sx={{ width: 50 }} />
+            {game.awayTeam.abbrev}
+          </Box>
+          <Typography>@</Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              textAlign: "center",
+              gap: 1,
+            }}
+          >
+            <Box component="img" src={game.homeTeam.logo} sx={{ width: 50 }} />
+            {game.homeTeam.abbrev}
+          </Box>
           <Typography>{dayjs(game.startTimeUTC).format("h:mm A")}</Typography>
         </CardContent>
       </CardActionArea>
